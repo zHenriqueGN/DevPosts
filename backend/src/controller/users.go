3d54163fde19f7ec5df5ed2161f5b988fc0ccbee
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2023-07-07 13:14:03 -0300

    feat: create method to insert a user in database

--- backend/src/controller/users.go
+++ backend/src/controller/users.go
@@ -1,10 +1,41 @@
 package controller
 
-import "net/http"
+import (
+	"api/src/database"
+	"api/src/models"
+	"api/src/repositories"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
 // CreateUser create a user in database
 func CreateUser(w http.ResponseWriter, r *http.Request) {
+	requestBody, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	var user models.User
+	if err := json.Unmarshal(requestBody, &user); err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := database.ConnectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	repository := repositories.NewUsersRepositorie(db)
+	ID, err := repository.Create(user)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(fmt.Sprintf("New user created with ID %d", ID)))
 }
 
 // FetchUsers fetch all the users in database
