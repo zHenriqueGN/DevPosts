2fe6e566021d14036a5de0c076034319b79ad40b
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2023-07-18 20:25:55 -0300

    feat: add jwt middleware

--- backend/internal/router/routes/routes.go
+++ backend/internal/router/routes/routes.go
@@ -1,7 +1,11 @@
 package routes
 
 import (
+	"api/internal/config"
+
+	jwtware "github.com/gofiber/contrib/jwt"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 // Representes all the API routes
@@ -13,11 +17,22 @@ type Route struct {
 }
 
 // Config register all the routes in the router
+// and applies the middlewares
 func Config(app *fiber.App) *fiber.App {
+	app.Use(logger.New())
+
 	api := app.Group("/api")
 
 	auth := api.Group("/auth")
 	auth.Add(LoginRoute.Method, LoginRoute.URI, LoginRoute.Func)
+
+	app.Use(
+		jwtware.New(
+			jwtware.Config{
+				SigningKey: jwtware.SigningKey{Key: []byte(config.SecretKey)},
+			},
+		),
+	)
 
 	users := api.Group("/users")
 	for _, route := range UserRoutes {
