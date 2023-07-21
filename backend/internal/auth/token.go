3691d67688532736d5fab317a7fbf9ee294985a2
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2023-07-20 22:12:55 -0300

    feat: create a function to return user id from JWTToken

--- backend/internal/auth/token.go
+++ backend/internal/auth/token.go
@@ -2,11 +2,13 @@ package auth
 
 import (
 	"api/internal/config"
+	"errors"
+	"fmt"
+	"strconv"
 	"strings"
 	"time"
 
-	jwt "github.com/dgrijalva/jwt-go"
-	"github.com/gofiber/fiber/v2"
+	jwt "github.com/golang-jwt/jwt/v5"
 )
 
 // GenerateToken creates a token with the given user permissions
@@ -17,13 +19,44 @@ func GenerateToken(userID int) (string, error) {
 		"userID":     userID,
 	}
 
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, permissions)
-	return token.SignedString([]byte(config.SecretKey))
+	JWTToken := jwt.NewWithClaims(jwt.SigningMethodHS256, permissions)
+	return JWTToken.SignedString([]byte(config.SecretKey))
 }
 
-// GetToken extracts the JWT token from request header and return it
-func GetToken(c *fiber.Ctx) (JWTToken string) {
-	AuthorizationValue := c.GetReqHeaders()["Authorization"]
-	JWTToken = strings.Split(AuthorizationValue, " ")[1]
+// GetUserID extracts the user ID from JWT token an return it
+func GetUserIDFromToken(authorization string) (userID int, err error) {
+	JWTTokenString := getTokenFromHeaders(authorization)
+	JWTToken, err := jwt.Parse(JWTTokenString, returnSecrectKey)
+	if err != nil {
+		return
+	}
+
+	if permissions, ok := JWTToken.Claims.(jwt.MapClaims); ok && JWTToken.Valid {
+		var ID int64
+
+		ID, err = strconv.ParseInt(fmt.Sprintf("%.f", permissions["userID"]), 10, 64)
+		if err != nil {
+			return
+		}
+
+		userID = int(ID)
+
+		return
+	}
+
+	err = errors.New("invalid token")
 	return
+}
+
+func getTokenFromHeaders(authorization string) (JWTToken string) {
+	JWTToken = strings.Split(authorization, " ")[1]
+	return
+}
+
+func returnSecrectKey(token *jwt.Token) (interface{}, error) {
+	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+		return nil, fmt.Errorf("unexpected signing method. received method: %v", token.Header["alg"])
+	}
+
+	return []byte(config.SecretKey), nil
 }
