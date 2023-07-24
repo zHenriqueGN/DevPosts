045a7acbdb7c90295cdcef59a17f18280e1a65e9
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2023-07-24 16:58:55 -0300

    fix: change jwtware alias

--- backend/internal/router/routes/routes.go
+++ backend/internal/router/routes/routes.go
@@ -3,7 +3,7 @@ package routes
 import (
 	"api/internal/config"
 
-	jwt "github.com/gofiber/contrib/jwt"
+	jwtware "github.com/gofiber/contrib/jwt"
 	"github.com/gofiber/fiber/v2"
 
 	"github.com/gofiber/fiber/v2/middleware/logger"
