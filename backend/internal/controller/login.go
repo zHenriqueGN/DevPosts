7f0366cd804456678236001cfd205c0fcd62f8ae
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2023-07-25 21:18:36 -0300

    feat: use fiber.Map in login controller

--- backend/internal/controller/login.go
+++ backend/internal/controller/login.go
@@ -3,7 +3,6 @@ package controller
 import (
 	"api/internal/auth"
 	"api/internal/database"
-	"api/internal/messages"
 	"api/internal/models"
 	"api/internal/repositories"
 	"api/internal/security"
@@ -16,12 +15,12 @@ func Login(c *fiber.Ctx) error {
 	var user models.User
 	err := c.BodyParser(&user)
 	if err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(messages.Error(err))
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err})
 	}
 
 	db, err := database.ConnectToDB()
 	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(messages.Error(err))
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err})
 	}
 	defer db.Close()
 
@@ -29,7 +28,7 @@ func Login(c *fiber.Ctx) error {
 
 	tempUser, err := repository.SearchByEmail(user.Email)
 	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(messages.Error(err))
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err})
 	}
 
 	if err := security.ComparePasswordWithHash(tempUser.Password, user.Password); err != nil {
@@ -38,8 +37,8 @@ func Login(c *fiber.Ctx) error {
 
 	token, err := auth.GenerateToken(tempUser.ID)
 	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(messages.Error(err))
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err})
 	}
 
-	return c.Status(fiber.StatusOK).JSON(messages.Message(token))
+	return c.Status(fiber.StatusOK).SendString(token)
 }
