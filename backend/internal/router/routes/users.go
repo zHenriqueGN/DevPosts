b901fdae17834f4a03ae511238a7169f6abf277a
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2023-07-26 23:05:46 -0300

    feat: create route to update password

--- backend/internal/router/routes/users.go
+++ backend/internal/router/routes/users.go
@@ -62,4 +62,10 @@ var UserRoutes = []Route{
 		Func:         controller.GetFollowings,
 		AuthRequired: true,
 	},
+	{
+		URI:          "/:id/update_passoword",
+		Method:       fiber.MethodPost,
+		Func:         controller.UpdatePassword,
+		AuthRequired: true,
+	},
 }
