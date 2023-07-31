12d4502e562792274dd179c7c7a6358693f5647d
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2023-07-31 11:50:48 -0300

    feat: divide the sql scripts in create and insert

--- backend/sql/create_tables.sql
+++ backend/sql/create_tables.sql
@@ -26,26 +26,3 @@ CREATE TABLE posts (
     likes INT NOT NULL DEFAULT 0,
     creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
-
-
-INSERT INTO users (
-    name,
-    username,
-    email,
-    password
-)
-VALUES
-    ('John Garfield', 'john.garfield', 'john.garfield@gmail.com', '$2a$10$xTKKshFe9EN9Fbp4O7ILp.RVdZDfaDcc9SL6dDK4QOtfUTcnSl0oW'),
-    ('Ali Rus', 'ali.rus', 'ali.rus@gmail.com', '$2a$10$xTKKshFe9EN9Fbp4O7ILp.RVdZDfaDcc9SL6dDK4QOtfUTcnSl0oW'),
-    ('Hellen Barch', 'hellen.barch', 'hellen.barch@gmail.com', '$2a$10$xTKKshFe9EN9Fbp4O7ILp.RVdZDfaDcc9SL6dDK4QOtfUTcnSl0oW');
-    
-INSERT INTO followers  (
-    user_id,
-    follower_id
-)
-VALUES
-    (1, 2),
-    (3, 1),
-    (1, 3);
-
