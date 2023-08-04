dd3c29492e69914ad9f2f8b21b2a776cd5fb0bba
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2023-08-04 15:00:20 -0300

    fix: ajust config to run in docker

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -2,18 +2,12 @@ package config
 
 import (
 	"fmt"
-	"log"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
 var (
 	// DBConn is the connection string to database
 	DBConn string
-
-	// APIPort is the port on the API will be running
-	APIPort string
 
 	// SecretKey is the key used to sign the web-token
 	SecretKey string
@@ -21,18 +15,11 @@ var (
 
 // LoadEnv loads the environment variables
 func LoadEnv() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error on loading .env file")
-	}
 	DBConn = fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
+		"host=db port=5432 user=%s password=%s dbname=%s sslmode=disable",
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_NAME"),
 	)
-	APIPort = os.Getenv("API_PORT")
 	SecretKey = os.Getenv("SECRET_KEY")
 }
